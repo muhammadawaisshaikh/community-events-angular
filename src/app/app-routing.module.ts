1216861abfe18f5ed4b6188545c90323afc7f2c2
Muhammad Awais <awaisshaikh94@gmail.com>
2021-10-01 23:05:19 +0500

    hot-fix after Haroon Ansari PR merge issue

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'auth', loadChildren: () => import('./components/auth/auth.module').then(m => m.AuthModule) }];
+const routes: Routes = [
+  { 
+    path: 'auth', 
+    loadChildren: () => import('./components/auth/auth.module').then(m => m.AuthModule) 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
