9f9db124b468ce9d76e689f461860feb5755692c
ilyasahmedshaikh <ilyasahmed081@gmail.com>
2021-10-03 15:56:33 +0500

    event card done

---
+++ src/app/shared/shared/components/event-card/event-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-event-card',
+  templateUrl: './event-card.component.html',
+  styleUrls: ['./event-card.component.scss']
+})
+export class EventCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/shared/shared/shared.module.ts
+++ src/app/shared/shared/shared.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ComponentsComponent } from './components/components.component';
+import { EventCardComponent } from './components/event-card/event-card.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ComponentsComponent,
+    EventCardComponent
+  ],
   imports: [
     CommonModule
   ]
