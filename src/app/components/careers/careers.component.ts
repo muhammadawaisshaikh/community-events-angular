d24335b1489613bced567c4ed249737d8216aaa4
ilyasahmedshaikh <ilyasahmed081@gmail.com>
2021-10-04 23:35:16 +0500

    about screen done

---
+++ src/app/components/careers/careers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-careers',
+  templateUrl: './careers.component.html',
+  styleUrls: ['./careers.component.scss']
+})
+export class CareersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
