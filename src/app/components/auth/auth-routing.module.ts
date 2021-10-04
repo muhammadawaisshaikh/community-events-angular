b76ab33255d7359aad80297451ddca99f0a257c8
Muhammad Idrees <47222718+muhammadidreesshaikh@users.noreply.github.com>
2021-10-04 21:59:39 +0500

    Feature/login 7 (#37)
    * login register and forget-password component added
    
    * add-login screen done

--- src/app/components/auth/auth-routing.module.ts
+++ src/app/components/auth/auth-routing.module.ts
@@ -2,7 +2,34 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth.component';
 
-const routes: Routes = [{ path: '', component: AuthComponent }];
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ForgetPasswordComponent } from './forget-password/forget-password.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AuthComponent,
+    children: [
+      {
+        path: '',
+        component: LoginComponent
+      },
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: 'register',
+        component: RegisterComponent
+      },
+      {
+        path: 'forget-password',
+        component: ForgetPasswordComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
