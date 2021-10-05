b75c79308a53958adb0f4659177b4e69f447dfdf
ilyasahmedshaikh <ilyasahmed081@gmail.com>
2021-10-05 23:24:56 +0500

    careers screen done

--- src/app/components/careers/careers.component.ts
+++ src/app/components/careers/careers.component.ts
@@ -7,6 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CareersComponent implements OnInit {
 
+  jobs: any = [
+    {
+      id: 1,
+      jobTitle: "Customer Experience"
+    },
+    {
+      id: 2,
+      jobTitle: "Data and Machine Learning"
+    },
+    {
+      id: 3,
+      jobTitle: "Graphic Design"
+    },
+    {
+      id: 4,
+      jobTitle: "Engineering"
+    },
+    {
+      id: 5,
+      jobTitle: "Enterprise"
+    },
+    {
+      id: 6,
+      jobTitle: "Marketing"
+    },
+    {
+      id: 7,
+      jobTitle: "People / Office"
+    },
+    {
+      id: 8,
+      jobTitle: "Product / Brand"
+    },
+    {
+      id: 9,
+      jobTitle: "Software Development"
+    },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
