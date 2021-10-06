c91adbac93720abc397b1b6761238038eb09c7c5
Greg Sithole <gregsithole@hotmail.co.za>
2021-10-06 22:12:59 +0200

    Fixed Routing and Added User Group Stepper to Module

--- src/app/components/user-group/user-group-routing.module.ts
+++ src/app/components/user-group/user-group-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserGroupComponent } from 'src/app/components/user-group/user-group/user-group.component';
+import { UserGroupStepperComponent } from 'src/app/components/user-group/user-group-stepper/user-group-stepper.component';
 
-const routes: Routes = [{ path: '', component: UserGroupComponent}];
+const routes: Routes = [{ path: '', component: UserGroupComponent, children: [
+    {path: '', component: UserGroupStepperComponent}
+  ]
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
