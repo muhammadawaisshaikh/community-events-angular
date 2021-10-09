07c2c949de1b829a495fc885708d3046ab74da39
Greg Sithole <gregsithole@hotmail.co.za>
2021-10-09 23:57:12 +0200

    Feature/issue 28/user group stepper tab (#50)
    * Created User Group Module
    
    * Lazy Loaded User Group Module into App Routing Module
    
    * Added User Group Component
    
    * Added User Group Routing Module
    
    * Added BS-Stepper to the project
    
    * Added User Group Stepper
    
    * Fixed Routing and Added User Group Stepper to Module

---
+++ src/app/components/user-group/user-group-stepper/user-group-stepper.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import Stepper from 'bs-stepper';
+
+@Component({
+  selector: 'app-user-group-stepper',
+  templateUrl: './user-group-stepper.component.html',
+  styleUrls: ['./user-group-stepper.component.scss']
+})
+export class UserGroupStepperComponent implements OnInit {
+
+  private stepper: Stepper | undefined
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.stepper = new Stepper(<Element>document.querySelector('#group-stepper'), {
+      linear: false,
+      animation: true
+    });
+  }
+
+  next() {
+    if (this.stepper) {
+      this.stepper.next();
+    }
+  }
+
+  previous() {
+    if (this.stepper) {
+      this.stepper.previous();
+    }
+  }
+
+
+
+}
--- src/app/components/user-group/user-group.module.ts
+++ src/app/components/user-group/user-group.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserGroupComponent } from './user-group/user-group.component';
+import { UserGroupStepperComponent } from './user-group-stepper/user-group-stepper.component';
+import { UserGroupRoutingModule } from 'src/app/components/user-group/user-group-routing.module';
 import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [
-    UserGroupComponent
+    UserGroupComponent,
+    UserGroupStepperComponent,
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    UserGroupRoutingModule
   ]
 })
 export class UserGroupModule { }
